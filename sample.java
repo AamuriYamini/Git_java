555532efa31fde2d2d920b121c845d01f3ea23d4
Yamini <aamuriyamini@gmail.com>
2022-06-22 08:58:51 +0530

    added new file sample

---
+++ sample.java
@@ -0,0 +1,14 @@
+import java.util.*;
+Class Sample
+{
+	public static void main(String args[])
+	{
+		int v=1;
+		int u=2;
+		static void myMethod()
+		{
+			int add=u+v;
+		}
+		System.out.println("sum=:"+add);
+	}
+}
